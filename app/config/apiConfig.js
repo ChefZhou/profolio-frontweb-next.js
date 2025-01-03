f5f293a86160181c093989e57d52b0b09e3093f2
ChefZhou <bob3345678@gmail.com>
2025-01-03 15:55:41 +0800

    api測試

--- app/config/apiConfig.js
+++ app/config/apiConfig.js
@@ -1,11 +1,3 @@
-if (!process.env.NEXT_PUBLIC_API_URL) {
-  console.error("警告: API URL 未設置");
-}
-
-if (!process.env.NEXT_PUBLIC_STRAPI_API_TOKEN) {
-  console.error("警告: API Token 未設置");
-}
-
 const apiConfig = {
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
@@ -13,11 +5,12 @@ const apiConfig = {
   apiPath: "/api",
   headers: {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_STRAPI_API_TOKEN}`,
+
     "Content-Type": "application/json",
   },
   endpoints: {
     articles: "/articles",
   },
 };
-
+console.log("Authorization:", apiConfig.headers.Authorization);
 export default apiConfig;
